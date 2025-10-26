96d9559e0991d64dfd8336be6f1388d7a879417a
Vibeke Ihlen Selnes <107354551+vibekeke@users.noreply.github.com>
2025-10-26 14:58:49 +0100

    tweak: final interaction cleanup

--- interaction/interaction_area.gd
+++ interaction/interaction_area.gd
@@ -1,14 +1,8 @@
 extends Area2D
 
-@export var area_size : Vector2 = Vector2(150,85)
-@onready var collision_shape = $CollisionShape2D
 #Denne kan overskrives av objectet vi putter InteractionArea inn i - men er i utgangspunktet tom.
 var interact: Callable = func():
 	pass
-
-func _ready():
-	pass
-	#collision_shape.disabled = true
 	
 func _on_area_entered(area: Area2D) -> void:
 	if area.name == "InteractionSensor":
