13de97e45680a6c3ee161dde3835f7f035e3b168
byllgrim <2606830-byllgrim@users.noreply.gitlab.com>
2025-10-26 15:47:10 +0100

    simplify main menu scripts

--- ui/check_box_start_game.gd
+++ ui/check_box_start_game.gd
@@ -1,15 +1,5 @@
 extends CheckBox
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-
 func _on_toggled(_toggled_on: bool) -> void:
 	SceneManager.change_scene("testB")
