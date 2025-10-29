943313be2a7f2fd85307e1ca1d556035d78f2885
Vibeke Ihlen Selnes <107354551+vibekeke@users.noreply.github.com>
2025-10-29 16:05:02 +0100

    fix: change player movement

--- player/player.gd
+++ player/player.gd
@@ -22,8 +22,8 @@ func _physics_process(delta):
 
 	if Globals.move_enabled:
 		direction = Vector2(
-			Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-			Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up"),
+			Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+			Input.get_action_strength("move_down") - Input.get_action_strength("move_up"),
 		)
 
 	if direction != Vector2.ZERO:
