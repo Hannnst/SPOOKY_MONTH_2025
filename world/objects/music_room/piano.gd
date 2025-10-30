d33a4a9cdecaf077bbef48e23c28b71b8f5617fd
Vibeke Ihlen Selnes <107354551+vibekeke@users.noreply.github.com>
2025-10-30 03:22:01 +0100

    wip: still working on piano room

---
+++ world/objects/music_room/piano.gd
@@ -0,0 +1,29 @@
+extends StaticBody2D
+
+# This is an informal interfrace for objects. Consider using classes in the future but avoiding complexity for now
+#Dette må legges til på alle interactable objekter:
+@onready var interaction_area = $InteractionArea
+@onready var dialogue = load("res://dialogues/music_room.dialogue")
+var piano_closed = false
+
+func _ready():
+	interaction_area.interact = Callable(self, "_on_interact")
+
+
+func _on_interact():
+	#Unik kode for hva som skjer når en spiller interacter
+	print("Interacting with:", self)
+	if Globals.trigger_finite_event("piano_girl"):
+		%LampLight.enabled = false
+		%AudioStreamPlayer2D.stop()
+		%PianoGirl.queue_free()
+	elif Globals.piano_closed == false:
+		InteractionManager.can_interact = false
+		DialogueManager.show_dialogue_balloon(dialogue, "piano")
+		await DialogueManager.dialogue_ended
+		if Globals.piano_closed == true:
+			Globals.trigger_finite_event("close_piano")
+			%PianoClosed.show()
+			print("Spooky loud noise??")
+	else:
+		DialogueManager.show_dialogue_balloon(dialogue, "piano_closed")
--- world/objects/music_room/tacky_lamp.gd
+++ world/objects/music_room/tacky_lamp.gd
@@ -1,9 +1,10 @@
-extends Sprite2D
+extends StaticBody2D
+
 
 # This is an informal interfrace for objects. Consider using classes in the future but avoiding complexity for now
 #Dette må legges til på alle interactable objekter:
 @onready var interaction_area = $InteractionArea
-
+@onready var dialogue = load("res://dialogues/music_room.dialogue")
 
 func _ready():
 	interaction_area.interact = Callable(self, "_on_interact")
@@ -12,11 +13,4 @@ func _ready():
 func _on_interact():
 	#Unik kode for hva som skjer når en spiller interacter
 	print("Interacting with:", self)
-	
-	var dialogue = load("res://dialogues/music_room.dialogue")
-	DialogueManager.show_dialogue_balloon(dialogue, "vindu")
-
-
-func activate():
-	# TODO: piano test activate function
-	print(self.name, " says pling plong :-)")
+	DialogueManager.show_dialogue_balloon(dialogue, "tacky_lamp")
