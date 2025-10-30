cbcc1ad3ee0e6a500b6d2ffa78915a6a9e6210cb
Vibeke Ihlen Selnes <107354551+vibekeke@users.noreply.github.com>
2025-10-30 15:17:58 +0100

    add: main hall and doors

---
+++ world/doors/door_masters.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+# This is an informal interfrace for objects. Consider using classes in the future but avoiding complexity for now
+
+#Dette må legges til på alle interactable objekter:
+@onready var interaction_area = $InteractionArea
+
+
+func _ready():
+	interaction_area.interact = Callable(self, "_on_interact")
+
+
+func _on_interact():
+	SceneManager.change_scene("master_bedroom")
