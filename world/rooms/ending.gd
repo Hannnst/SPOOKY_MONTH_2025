8ff23b2a443b12fe62db8dfa1f33bb84c723b964
Vibeke Ihlen Selnes <107354551+vibekeke@users.noreply.github.com>
2025-10-31 17:47:14 +0100

    wooop

--- world/rooms/ending.gd
+++ world/rooms/ending.gd
@@ -2,9 +2,15 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	$AnimationPlayer.play("fade_in")
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func play_ending_dialogue():
+	var dialogue = load("res://dialogues/ending_dialogue.dialogue")
+	DialogueManager.show_dialogue_balloon(dialogue, "ending")
+	await DialogueManager.dialogue_ended
+	return_to_main()
+
+func return_to_main():
+	SceneManager.change_scene("main_menu")
