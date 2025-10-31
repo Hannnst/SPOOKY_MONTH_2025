024aa394cb9e2c6ba208c2f966dcbba99efc1f70
Vibeke Ihlen Selnes <107354551+vibekeke@users.noreply.github.com>
2025-10-31 17:27:27 +0100

    add correct teleporters

--- autoloads/InventoryManager.gd
+++ autoloads/InventoryManager.gd
@@ -6,12 +6,12 @@ signal inventory_updated #Used to alert the UI that the inventory changed
 signal focus_returned
 
 var inventory = [
-	{ "name": "notebook", "is_owned": true, "texture": preload("res://assets/items/lapp-icon.png") },
-	{ "name": "key", "is_owned": true, "texture": preload("res://world/objects/outside/key.webp"), "target_name": "Gate" },
-	{ "name": "toy", "is_owned": true, "texture": preload("res://assets/items/tamagotchi.webp") },
-	{ "name": "skull", "is_owned": true, "texture": preload("res://assets/items/skull_front.webp"), "target_name": "Grave" },
-	{ "name": "vinyl", "is_owned": true, "texture": preload("res://assets/furniture/vinyl.webp"), "target_name": "VinylPlayer" },
-	{ "name": "matches", "is_owned": true, "texture": preload("res://assets/items/matches.webp") },
+	{ "name": "notebook", "is_owned": false, "texture": preload("res://assets/items/lapp-icon.png") },
+	{ "name": "key", "is_owned": false, "texture": preload("res://world/objects/outside/key.webp"), "target_name": "Gate" },
+	{ "name": "toy", "is_owned": false, "texture": preload("res://assets/items/tamagotchi.webp") },
+	{ "name": "skull", "is_owned": false, "texture": preload("res://assets/items/skull_front.webp"), "target_name": "Grave" },
+	{ "name": "vinyl", "is_owned": false, "texture": preload("res://assets/furniture/vinyl.webp"), "target_name": "VinylPlayer" },
+	{ "name": "matches", "is_owned": false, "texture": preload("res://assets/items/matches.webp") },
 ]
 var current_slot_index: int = 0 #Keeps track of which item the player last selected, across scene change.
 
